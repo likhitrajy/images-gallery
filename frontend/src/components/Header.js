faa4fe35e9805293bfc3509dc3f8a680dd530b77
likhitrajy <likhitrajy@gmail.com>
2023-08-09 08:07:14 -0400

    Fixed code with linter

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
-const Header = (props) =>{
-    const titleStyle = {
-        fontSize: '30px', 
-    };
-    return (
-        <Navbar bg="dark" data-bs-theme="dark">
-        <Container className="justify-content-center">
-        <Navbar.Brand href="/" style={titleStyle}>{props.title}</Navbar.Brand>
-        </Container>
-    </Navbar>)
+const Header = (props) => {
+  const titleStyle = {
+    fontSize: '30px',
+  };
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container className="justify-content-center">
+        <Navbar.Brand href="/" style={titleStyle}>
+          {props.title}
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,26 +5,32 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const Search = ({word, setWord, handleSubmit}) =>{
-    return (
-        <Container className="mt-4">
-        <Row className="justify-content-center">
-         <Col xs = {15} md = {8}>   
-         <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col xs ={10}><Form.Control 
-            placeholder="Search for images..." 
-            type = "text"
-            value = {word}
-            onChange={(e) => setWord(e.target.value)}
-            /></Col>
-            <Col><Button variant="success" type="submit">Search</Button></Col>
-          </Row>
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={15} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={10}>
+                <Form.Control
+                  placeholder="Search for images..."
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Button variant="success" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Row>
           </Form>
-          </Col>
-          </Row>
-        </Container>
-      );
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
