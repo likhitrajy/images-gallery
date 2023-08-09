c71de0f748982c7e97135a1856427b50172c107b
likhitrajy <likhitrajy@gmail.com>
2023-08-09 08:00:39 -0400

    Eslint and Prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import { useState } from 'react';
 
-  const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-  const App = () => {
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const App = () => {
   const [word, setWord] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((res)=>res.json())
-      .then((data)=>{
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err);
-      })
-      setWord('');
+      });
+    setWord('');
   };
-  
+
   return (
     <div>
       <Header title="IMAGES GALLERY" />
