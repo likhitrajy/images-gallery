885ef081318443834d14a8c2621fcb10295aaf50
likhitrajy <likhitrajy@gmail.com>
2023-08-16 07:19:45 -0400

    Added linting and formatting (in progress)

--- api/main.py
+++ api/main.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
-UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY","")
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please check UNSPLASH_KEY authentication.")
@@ -15,17 +15,19 @@ app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
     headers = {
-        "Authorization": "Client-ID "+ UNSPLASH_KEY,
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
         "Accept-Version": "v1",
-
     }
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
