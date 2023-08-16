a6d62b2b0a7d9663fe704967348bc03b24b15d3a
likhitrajy <likhitrajy@gmail.com>
2023-08-16 05:39:11 -0400

    Added request to UNSPLASH API

--- api/main.py
+++ api/main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="./.env.local")
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY","")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please check UNSPLASH_KEY authentication.")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {
+        "Authorization": "Client-ID "+ UNSPLASH_KEY,
+        "Accept-Version": "v1"
+    }
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
